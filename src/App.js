d32626c0a03c3cd04f6f80534acd09d98d6e1811
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-16 12:51:58 +0530

    V2 (#2)
    * rem move babelrc
    
    * add v2
    
    * add package rudux react-redux
    
    * add package-lock.json
    
    * cp actions components containers reducers to v2
    
    * add redux store and provider
    
    * remove unused App.css logo.svg
    
    * moved src
    
    * moved code component App.js to App.js
    
    * working version
    
    * add proptypes
    
    * add Prop types to Hello
    
    * remove unused index.css
    
    * remove console.logs
    
    * remove files and folder to restructure
    
    * add yarn instruction
    
    * add current year to LICENSE
    
    * mv v2/* ./

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import HelloWorld from './containers/HelloWorld'
+
+class App extends Component {
+  render() {
+    return (
+      <HelloWorld />
+    );
+  }
+}
+
+export default App;
