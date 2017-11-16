a6b8488af8e30cceabad9b2ea8ec6724c6debe73
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-16 12:27:26 +0530

    moved code component App.js to App.js

--- v2/src/App.js
+++ v2/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import HelloWorld from './containers/HelloWorld'
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <HelloWorld />
     );
   }
 }
