7b1a4b9de5bacad28ce03f150f7a029c270db484
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-18 10:17:08 +0530

    add initialState and add RESET reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux'
-import { HELLO_WORLD } from './../actions'
+import { HELLO_WORLD, RESET } from './../actions'
 
-const helloWorld = (state = { message: 'Hello' }, action) => {
+let initialState = { message: 'Hello' }
+
+const helloWorld = (state=initialState, action) => {
   switch (action.type) {
     case HELLO_WORLD:
       return Object.assign({}, state, { message: 'Hello, World!' })
+    case RESET:
+    	return state = initialState 
     default:
       return state
   }
