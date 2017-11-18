f9b5ebabb0983ba0c1e70f845c9ab4baf4a7533d
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-18 10:06:49 +0530

    add reset

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,14 @@
 export const HELLO_WORLD = 'HELLO_WORLD'
+export const RESET = 'RESET'
 
 export const helloWorld = () => {
   return {
     type: HELLO_WORLD
+  }
+}
+
+export const reset = () => {
+  return {
+    type: RESET
   }
 }
